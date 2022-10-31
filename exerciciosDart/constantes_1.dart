54c0d59e530757f762f2fd6d8b19b9a5fed7c2cb
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2022-10-31 20:53:12 -0300

    const

--- exerciciosDart/constantes_1.dart
+++ exerciciosDart/constantes_1.dart
@@ -1,7 +1,17 @@
 import 'dart:io';
 
 main() {
-  // como calcular a área da circunferência Pi * raio * raio //
-  final text = stdin.readLineSync();
-  print(text);
+  // stdout printa a tela sem que haja quebra de linha, mostra ao lado //
+
+  stdout.write("Insert the value: ");
+
+  var userImput = stdin.readLineSync();
+
+  // com o final dertermino que o raio é uma constante e não vai ser alterada! //
+
+  final double n1 = double.parse(userImput!);
+
+  // const: constante que pode ser definida na compilação, não no runtime //
+
+  print("O valor inserido é: " + n1.toString());
 }
