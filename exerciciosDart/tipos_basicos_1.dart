b0890e4101eb091bfcce72510cc9c71e560793e1
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2022-10-31 22:04:57 -0300

    novas classes

---
+++ exerciciosDart/tipos_basicos_1.dart
@@ -0,0 +1,18 @@
+/*
+- Números (int e double)
+- String (String)
+- Booleano (bool)
+*/
+
+void main() {
+  int n1 = 5;
+  double n2 = -3.23.abs();
+  double n3 = double.parse("11.245");
+  var n4 = n1 + n2;
+  num n5 = 66;
+  n5 = 6.8;
+  print(n1 + n3);
+  print(n4);
+  print(n4.runtimeType);
+  print(n3.runtimeType);
+}
