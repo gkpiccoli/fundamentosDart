f1aba19aa3e1f4a68a3ca38c82022580b0d84f50
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2022-11-05 16:43:06 -0300

    const2

--- constantes_2.dart
+++ constantes_2.dart
@@ -1,5 +1,21 @@
 main() {
+  // var = Variável, consigo atribuir novos valores.
   var list = ['Beto', 'Pame', 'Wes'];
   list.add('Gus');
   print(list);
+
+  list = ['PineApple', 'Onions', 'Mushrooms'];
+  print(list);
+
+//Se fosse final, não conseguiria editar.
+  var list2 = const ['Dog', 'Cat', 'Dinossaur'];
+
+  print(list2);
+
+  // Como ela é constante, não pode ser alterada.
+  //list2.add('Chicken');
+  print(list2);
+
+  list2 = ['Egg', 'Coffee'];
+  print(list2);
 }
