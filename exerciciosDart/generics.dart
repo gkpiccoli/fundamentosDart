220a7a061bf465a10db8493001ab4552d2b32b12
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2022-11-08 11:20:52 -0300

    generics

--- exerciciosDart/generics.dart
+++ exerciciosDart/generics.dart
@@ -1,3 +1,8 @@
 main() {
-  
+  List<String> clones = ['cloen1', 'clone1', 'conel1', 'cone1l'];
+  clones.add('collon1');
+  print(clones);
+
+  Map<String, double> goodies = {'bala': 1, 'doce': 2.23, 'agua': 1.75};
+  print(goodies);
 }
