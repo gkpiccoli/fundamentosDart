15af72b95c019d800e68baf0b09d92653f0a0912
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-11-26 14:58:27 -0300

    orientacao a objetos

--- exerciciosDart/oo/class_data.dart
+++ exerciciosDart/oo/class_data.dart
@@ -1,3 +1,19 @@
-class SimpleData {
-    
+class bDay {
+  late int day;
+  late int month;
+  late int year;
+}
+
+// Classe é o projeto, molde, à partir dele você consegue criar objetos;
+// Pode-se instanciar quantos objetos quiser;
+// Late modifier
+
+void main() {
+  var birthDay = new bDay();
+// new não é obrigatório
+  birthDay.day = 03;
+  birthDay.month = 09;
+  birthDay.year = 1992;
+
+  print("${birthDay.day}/${birthDay.month}/${birthDay.year}");
 }
