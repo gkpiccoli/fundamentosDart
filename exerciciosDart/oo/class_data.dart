02959050bbc6bba5d2a5a05625981d748f6ef315
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-11-26 13:58:30 -0300

    classes

--- exerciciosDart/oo/class_data.dart
+++ exerciciosDart/oo/class_data.dart
@@ -1,12 +1,3 @@
-main(){
-  Person somePerson = newPerson();
-  somePerson.firstName; 
-}
-class Person{
-  String firstName;
-  String middleName;
-  String lastName;
-  
-  String getFullName () => "$firstName $middleName $lastName";
-  print(getFullName)
+class SimpleData {
+    
 }
