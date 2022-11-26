5ca33d84de7de1334c226e6da46ce99493c5f92c
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-11-26 16:26:11 -0300

    classes e metodos

--- exerciciosDart/oo/class_data.dart
+++ exerciciosDart/oo/class_data.dart
@@ -14,11 +14,13 @@ class Data {
 
 // Classe é o projeto, molde, à partir dele você consegue criar objetos;
 // Pode-se instanciar quantos objetos quiser;
-// Late modifier
+// Dentro de uma classe, uma função é chamada de mét0d0;
 
 void main() {
   var birthDay = new Data();
-// new não é obrigatório
+// new não é obrigatório;
+// objeto do tipo Data;
+// a variavel aniversário é um exemplo de classe, que contem objetos dia, mes, ano;
   birthDay.day = 03;
   birthDay.month = 09;
   birthDay.year = 1992;
@@ -33,6 +35,7 @@ void main() {
   // print("${buyDate.day}/${buyDate.month}/${buyDate.year}");
 
   String f1 = birthDay.formatted();
+  // data formatada
 
   print("O dia do aniversário é $f1");
   print("Data aniver Pami ${buyDate.formatted()} ");
