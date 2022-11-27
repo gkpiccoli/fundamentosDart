8c67757ced622ca77963fa74cd30cfb080d0a34c
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-11-27 12:48:03 -0300

    p. nom

--- exerciciosDart/oo/constructors.dart
+++ exerciciosDart/oo/constructors.dart
@@ -8,11 +8,12 @@ class Data {
   //   this.month = initMonth;
   //   this.year = initYear;
 
-  
+// Parametros nomeados
+  Data({this.day = 1, this.month = 3, this.year = 1994});
+
   // }
 
   //Data(this.day, this.month, this.year);
-  Data([this.day = 1, this.month = 3, this.year = 1994]);
 
   String formatted() {
     return "$day/$month/$year";
@@ -24,6 +25,9 @@ void main() {
   birthday.day = 12;
   birthday.month = 12;
   birthday.year = 1992;
+
+  var dataFinal = Data(2, 5, 6);
+  print(dataFinal);
 
   print(birthday);
   print(birthday.toString());
